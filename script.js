1736c0faf071ab1646a9823d411f33a4e970e515
togrulbadirov <togrul4242@gmail.com>
2023-10-15 19:58:21 +0400

    First commit.

---
+++ script.js
@@ -0,0 +1,68 @@
+//---------------------------------------------------- Task 1
+
+
+// const arr = [1, 22, 24, 2, 25, 2, 6, 2];
+// let sum = 0;
+
+// arr.sort((a, b) => a - b);
+
+
+// for (let i = 1; i < arr.length-1; i++) {
+//     const element = arr[i];
+//     sum+=element;
+
+// }
+// console.log(sum);
+
+// ------------------------------------------------ Task2
+
+// const arr = [1, 22, 24, 2,25,2, 6,2];
+// let num = 2;
+// let count = 0;
+// arr.forEach(element => {
+//     if (element === num) {
+//         count++;
+//     }
+// });
+// console.log(count);
+
+
+//------------------------------------- Task3
+
+// const arr = [1, 22, 24, 25, 6];
+// let max = arr[0];
+// let index = 0;
+// let sum = 0;
+// arr.forEach((element, i) => {
+//     if (element > max) {
+//         max = element;
+//         index = i;
+//     }
+// });
+
+// for (let i = 1; i < + index; i++) {
+//     const element = arr[i];
+//     sum += element
+
+// }
+// console.log("sum", sum);
+
+
+//---------------------------------------------------- Task 4
+
+// const arr = [1, 22, 24, 5, 6];
+// let max = arr[0];
+// let min = arr[0];
+// let edor = 0;
+// arr.forEach(element => {
+//     if (element > max) {
+//         max = element;
+//     }
+//     else if (element < min) {
+//         min = element;
+//     }
+// });
+// edor = (max + min) / 2
+// console.log(edor);
+
+
